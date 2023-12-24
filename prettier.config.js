27c07c1c1d585e37aaf9745bf893d518792d9833
GodwhiteTaiwan <89009069+GodwhiteTaiwan@users.noreply.github.com>
2023-12-24 10:56:09 +0800

    update

--- prettier.config.js
+++ prettier.config.js
@@ -3,13 +3,6 @@ module.exports = {
 	singleQuote: true,
 	trailingComma: "none",
 	arrowParens: "avoid",
-	plugins: [
-		"prettier-plugin-tailwindcss",
-		// for sort fields in package.json
-		"prettier-plugin-pkg",
-		// for sorting imports
-		"@ianvs/prettier-plugin-sort-imports",
-	],
 	overrides: [
 		{
 			files: "*.svg",
