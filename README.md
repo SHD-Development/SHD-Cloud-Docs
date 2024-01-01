362316cba253f07bb1bce23cf891d50a96bc6594
GodwhiteTaiwan <89009069+GodwhiteTaiwan@users.noreply.github.com>
2024-01-01 17:20:46 +0800

    一些排版

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SHD Cloud 文檔
+
 不會編輯的麻煩查看 https://docs.shdcloud.xyz/docs
--- prettier.config.js
+++ prettier.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-	semi: false,
-	singleQuote: true,
-	trailingComma: "none",
-	arrowParens: "avoid",
-	overrides: [
-		{
-			files: "*.svg",
-			options: {
-				parser: "html",
-			},
-		},
-	],
-	proseWrap: "always", // printWidth line breaks in md/mdx
-};
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  overrides: [
+    {
+      files: '*.svg',
+      options: {
+        parser: 'html'
+      }
+    }
+  ],
+  proseWrap: 'always' // printWidth line breaks in md/mdx
+}
